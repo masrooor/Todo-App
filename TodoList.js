78601b1d4bb4c1093fd44b6f193be2a1dfe59b90
masrooor <19424169+masrooor@users.noreply.github.com>
2019-01-03 17:26:17 +0500

    Update TodoList.js

--- TodoList.js
+++ TodoList.js
@@ -1,16 +1,5 @@
 import React from 'react';
-/*class TodoList extends React.Component {
-  render(props) {
-    return (
-      <ul>
-        {props.items.map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-    );
-  }
-}
-export default TodoList;*/
+
 const Todolist = (props) =>{
 	return (
 			<li>  <span > {props.items} </span>
